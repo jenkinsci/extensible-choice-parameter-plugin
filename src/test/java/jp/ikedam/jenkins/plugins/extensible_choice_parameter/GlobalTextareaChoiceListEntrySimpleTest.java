36c83fa1bbfad1492d4f21ebb5f1801878483cd9
ikedam <devld@ikedam.jp>
2013-01-04 17:30:01 +0900

    Write a test form TextareaStringListUtility.

--- src/test/java/jp/ikedam/jenkins/plugins/extensible_choice_parameter/GlobalTextareaChoiceListEntrySimpleTest.java
+++ src/test/java/jp/ikedam/jenkins/plugins/extensible_choice_parameter/GlobalTextareaChoiceListEntrySimpleTest.java
@@ -3,6 +3,7 @@
  */
 package jp.ikedam.jenkins.plugins.extensible_choice_parameter;
 
+
 import java.util.Arrays;
 
 import junit.framework.TestCase;
@@ -112,192 +113,20 @@ public class GlobalTextareaChoiceListEntrySimpleTest extends TestCase
         }
     }
     
-    public void testGlobalTextareaChoiceListEntry_choiceList()
+    public void testGlobalTextareaChoiceListEntry_choiceListText()
     {
-        // Easy case.
-        {
-            String text = "a\nb\nc";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals(Arrays.asList("a", "b", "c"), entry.getChoiceList());
-        }
-        
-        // null
-        {
-            String text = null;
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals(0, entry.getChoiceList().size());
-        }
-        
-        // Empty
-        {
-            String text = "";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals(0, entry.getChoiceList().size());
-        }
-        
-        // Empty line in the middle.
-        {
-            String text = "a\n\nc";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals(Arrays.asList("a", "", "c"), entry.getChoiceList());
-        }
-        
-        // Multiple empty line in the middle.
-        {
-            String text = "a\n\n\nc";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals(Arrays.asList("a", "", "", "c"), entry.getChoiceList());
-        }
-        
-        // Empty line in the beginning.
-        {
-            String text = "\na\nb\nc";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals(Arrays.asList("", "a", "b", "c"), entry.getChoiceList());
-        }
-        
-        // Multiple empty line in the beginning.
-        {
-            String text = "\n\na\nb\nc";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals(Arrays.asList("", "", "a", "b", "c"), entry.getChoiceList());
-        }
-        
-        // Empty line in the end.
-        {
-            String text = "a\nb\nc\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals(Arrays.asList("a", "b", "c"), entry.getChoiceList());
-        }
-        
-        // Multiple empty line in the end.
-        {
-            String text = "a\nb\nc\n\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Multiple empty line in the end.",
-                    Arrays.asList("a", "b", "c", ""),
-                    entry.getChoiceList()
-            );
-        }
-        
-        // Multiple empty line only
-        {
-            String text = "\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Multiple empty line only",
-                    Arrays.asList(""),
-                    entry.getChoiceList()
-            );
-        }
-        
-        // Newline code is CR LF.
-        {
-            String text = "a\r\nb\r\nc";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Newline code is CR LF.",
-                    Arrays.asList("a", "b", "c"),
-                    entry.getChoiceList()
-            );
-        }
-        
-        
-        // Newline code is mixed.
-        {
-            String text = "a\nb\r\nc";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Newline code is mixed.",
-                    Arrays.asList("a", "b", "c"),
-                    entry.getChoiceList()
-            );
-        }
-        
-        
-        // Newline code is mixed, and continued
-        {
-            String text = "a\n\r\n\nc";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Newline code is mixed, and continued", 
-                    Arrays.asList("a", "", "", "c"), entry.getChoiceList());
-        }
-    }
-    
-    public void testGetChoiceListText()
-    {
-        // Easy case.
-        {
-            String text = "a\nb\nc\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Easy case.", text, entry.getChoiceListText());
-        }
-        
-        // Last line is not terminated with LF.
-        {
-            String text = "a\nb\nc";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Last line is not terminated with LF.", text + "\n", entry.getChoiceListText());
-        }
-        
-        // Empty
-        {
-            String text = "";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Empty", text, entry.getChoiceListText());
-        }
-        
-        // Empty choice in the middle.
-        {
-            String text = "a\n\nc\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Empty choice in the middle.", text, entry.getChoiceListText());
-        }
-        
-        // Multiple empty choices in the middle.
-        {
-            String text = "a\n\n\nc\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Multiple empty choices in the middle.", text, entry.getChoiceListText());
-        }
-        
-        // Empty choice in the beginning.
-        {
-            String text = "\na\nb\nc\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Empty choice in the beginning.", text, entry.getChoiceListText());
-        }
-        
-        // Multiple empty choices in the beginning.
-        {
-            String text = "\n\na\nb\nc\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Multiple empty choices in the beginning.", text, entry.getChoiceListText());
-        }
-        
-        // Empty choice in the end.
-        {
-            String text = "a\nb\nc\n\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Empty choice in the end.", text, entry.getChoiceListText());
-        }
-        
-        // Multiple empty choice in the end.
-        {
-            String text = "a\nb\nc\n\n\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Multiple empty choice in the end.", text, entry.getChoiceListText());
-        }
-        
-        // Empty choice only
-        {
-            String text = "\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Empty choice only", text, entry.getChoiceListText());
-        }
-        
-        // Multiple empty choice only
-        {
-            String text = "\n\n";
-            GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", text);
-            assertEquals("Multiple empty choice only", text, entry.getChoiceListText());
-        }
+        // complete tests are done in TextareaStringListUtilityTest.
+        String choiceListText = "a\nb\nc\n";
+        GlobalTextareaChoiceListEntry entry = new GlobalTextareaChoiceListEntry("test", choiceListText);
+        assertEquals(
+                "Basic test of choiceListText in GlobalTextareaChoiceListEntry()",
+                Arrays.asList("a", "b", "c"),
+                entry.getChoiceList()
+        );
+        assertEquals(
+                "Basic test of getChoiceListText()",
+                choiceListText,
+                entry.getChoiceListText()
+        );
     }
 }
---
+++ src/test/java/jp/ikedam/jenkins/plugins/extensible_choice_parameter/utility/TextareaStringListUtilityTest.java
@@ -0,0 +1,205 @@
+/**
+ * 
+ */
+package jp.ikedam.jenkins.plugins.extensible_choice_parameter.utility;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for TextareaStringListUtility
+ */
+public class TextareaStringListUtilityTest extends TestCase
+{
+    public void testStringListFromTextarea()
+    {
+        // Easy case
+        {
+            String text = "a\nb\nc";
+            List<String> expected = Arrays.asList("a", "b", "c");
+            assertEquals("Easy case", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // null
+        {
+            String text = null;
+            List<String> expected = new ArrayList<String>(0);
+            assertEquals("null", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Empty
+        {
+            String text = "";
+            List<String> expected = new ArrayList<String>(0);
+            assertEquals("Empty", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Empty line in the middle.
+        {
+            String text = "a\n\nc";
+            List<String> expected = Arrays.asList("a", "", "c");
+            assertEquals("Empty line in the middle", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Multiple empty line in the middle.
+        {
+            String text = "a\n\n\nc";
+            List<String> expected = Arrays.asList("a", "", "", "c");
+            assertEquals("Multiple empty line in the middle", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Empty line in the beginning.
+        {
+            String text = "\na\nb\nc";
+            List<String> expected = Arrays.asList("", "a", "b", "c");
+            assertEquals("Empty line in the beginning", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Multiple empty line in the beginning.
+        {
+            String text = "\n\na\nb\nc";
+            List<String> expected = Arrays.asList("", "", "a", "b", "c");
+            assertEquals("Multiple empty line in the beginning", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Empty line in the end.
+        {
+            String text = "a\nb\nc\n";
+            List<String> expected = Arrays.asList("a", "b", "c");
+            assertEquals("Empty line in the end", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Multiple empty line in the end.
+        {
+            String text = "a\nb\nc\n\n";
+            List<String> expected = Arrays.asList("a", "b", "c", "");
+            assertEquals("Multiple empty line in the end", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Multiple empty line only
+        {
+            String text = "\n";
+            List<String> expected = Arrays.asList("");
+            assertEquals("Multiple empty line only", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Newline code is CR LF.
+        {
+            String text = "a\r\nb\r\nc";
+            List<String> expected = Arrays.asList("a", "b", "c");
+            assertEquals("Newline code is CR LF.", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Newline code is mixed.
+        {
+            String text = "a\nb\r\nc";
+            List<String> expected = Arrays.asList("a", "b", "c");
+            assertEquals("Newline code is mixed.", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // Newline code is mixed, and occurred in sequence
+        {
+            String text = "a\n\r\n\nc";
+            List<String> expected = Arrays.asList("a", "", "", "c");
+            assertEquals("Newline code is mixed, and occurred in sequence.", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+        
+        // lines containing white spaces
+        {
+            String text = " a\nb \n c \n ";
+            List<String> expected = Arrays.asList(" a", "b ", " c ", " ");
+            assertEquals("lines containing white spaces.", expected, TextareaStringListUtility.stringListFromTextarea(text));
+        }
+    }
+    
+    public void testTextareaFromStringList()
+    {
+        // Easy case.
+        {
+            List<String> lst = Arrays.asList("a", "b", "c");
+            String expected = "a\nb\nc\n";
+            assertEquals("Easy case.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // null
+        {
+            List<String> lst = null;
+            String expected = "";
+            assertEquals("Empty.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Empty
+        {
+            List<String> lst = new ArrayList<String>(0);
+            String expected = "";
+            assertEquals("Empty.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Empty string in the middle.
+        {
+            List<String> lst = Arrays.asList("a", "", "c");
+            String expected = "a\n\nc\n";
+            assertEquals("Empty string in the middle.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Multiple empty strings in the middle.
+        {
+            List<String> lst = Arrays.asList("a", "", "", "c");
+            String expected = "a\n\n\nc\n";
+            assertEquals("Multiple empty strings in the middle.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Empty string in the beginning.
+        {
+            List<String> lst = Arrays.asList("", "a", "b", "c");
+            String expected = "\na\nb\nc\n";
+            assertEquals("Empty string in the beginning.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Multiple empty strings in the beginning.
+        {
+            List<String> lst = Arrays.asList("", "", "a", "b", "c");
+            String expected = "\n\na\nb\nc\n";
+            assertEquals("Multiple empty strings in the beginning.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Empty string in the end.
+        {
+            List<String> lst = Arrays.asList("a", "b", "c", "");
+            String expected = "a\nb\nc\n\n";
+            assertEquals("Empty string in the end.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Multiple empty strings in the end.
+        {
+            List<String> lst = Arrays.asList("a", "b", "c", "", "");
+            String expected = "a\nb\nc\n\n\n";
+            assertEquals("Multiple empty strings in the end.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Empty string only
+        {
+            List<String> lst = Arrays.asList("");
+            String expected = "\n";
+            assertEquals("Empty string only.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Multiple empty strings only
+        {
+            List<String> lst = Arrays.asList("", "");
+            String expected = "\n\n";
+            assertEquals("Multiple empty strings only.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+        // Strings containing white spaces
+        {
+            List<String> lst = Arrays.asList(" ", " a", " ", "b ", " c ", " ");
+            String expected = " \n a\n \nb \n c \n \n";
+            assertEquals("Multiple empty strings only.", expected, TextareaStringListUtility.textareaFromStringList(lst));
+        }
+        
+    }
+}
