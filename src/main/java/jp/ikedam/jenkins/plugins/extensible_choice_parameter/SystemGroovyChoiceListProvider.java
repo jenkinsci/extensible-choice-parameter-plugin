560d0ff68b12f5dc971c6eb423802c1a31425ac7
ikedam <devld@ikedam.jp>
2013-04-10 21:26:39 +0900

    refs #21 changed copyright notices. SystemGroovyChoiceListProvider is a work of Michael Rumpf.

--- src/main/java/jp/ikedam/jenkins/plugins/extensible_choice_parameter/SystemGroovyChoiceListProvider.java
+++ src/main/java/jp/ikedam/jenkins/plugins/extensible_choice_parameter/SystemGroovyChoiceListProvider.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  * 
- * Copyright (c) 2012-2013 IKEDA Yasuyuki
+ * Copyright (c) 2013 Michael Rumpf
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
